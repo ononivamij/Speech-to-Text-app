54fae7427b1f6219bc12263b38b18134728ec696
jonathan <jonathanononivami@gmail.com>
2025-05-01 22:50:26 +0100

    still working

--- script.js
+++ script.js
@@ -16,11 +16,38 @@ function startConverting(){
 
 
 function setupRecognition(recognition){
+    recognition.continuous = true;
+
+    recognition.interimResults = true;
+
+    recognition.lang = "en-Us"; 
+
+    recognition.onresult = function(event){
+
+        //ProcessResult()
+
+        processResult(event.results);
+
+    }
+
 
 }
 
 
 function processResult(results){
+
+    let finalTranscript = "";
+    let interTranscript = "";
+
+    for(let i = 0; i < results.length; i++){
+
+        let transcript = results[1][0].transcript;
+        transcript.replace("\n","<br>");
+
+        
+
+    }
+
 
 }
 
